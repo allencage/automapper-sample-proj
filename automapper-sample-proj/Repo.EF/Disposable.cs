aaba4d900f3015443bfe8c03bdf781fa7d45720b
allencage <alingabriel.ciuca@gmail.com>
2017-04-14 21:14:03 +0300

    Implemented UnitOfWork pattern

---
+++ automapper-sample-proj/Repo.EF/Disposable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.EF
+{
+    public class Disposable : IDisposable
+    {
+
+        private bool _disposedValue;
+
+        /// <summary>
+        /// Releases unmanaged resources and performs other cleanup operations before the
+        /// <see cref="Disposable"/> is reclaimed by garbage collection.
+        /// </summary>
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        private void Dispose(bool disposing)
+        {
+            if (_disposedValue) return;
+            if (disposing)
+            {
+                DisposeCore();
+            }
+            _disposedValue = true;
+            GC.SuppressFinalize(this);
+        }
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        protected virtual void DisposeCore()
+        {
+
+        }
+
+    }
+}
---
+++ automapper-sample-proj/Repo.EF/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Log.Contracts;
+using Repo.Contracts;
+using System;
+using System.Data.Entity;
+
+namespace Repo.EF
+{
+    public class UnitOfWork : Disposable, IUnitOfWork
+    {
+        private ICustomLog _logger;
+        private DbContext _dbContext;
+        public DbContext DataContext => _dbContext ?? (_dbContext = new DataContext());
+
+        private UnitOfWork(ICustomLog logger)
+        {
+            _logger = logger;
+        }
+
+        public static UnitOfWork CreateUnitOfWorkWithCustomLog(ICustomLog logger)
+        {
+            return new UnitOfWork(logger);
+        }
+
+        public void Commit()
+        {
+            try
+            {
+                DataContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "The was a problem updating the db.");
+            }
+        }
+    }
+}
